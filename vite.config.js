0a177036ad554ff36a4f422642ead3bede2ce9a6
seankalb <153946850+Skalb8865@users.noreply.github.com>
2025-04-30 16:46:49 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // Main Pages
+        main: resolve(__dirname, 'index.html'),
+        pokemondb: resolve(__dirname, 'pokemon-db.html'),
+      },
+    },
+  },
+})
