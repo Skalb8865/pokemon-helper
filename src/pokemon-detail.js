a93b4db55162db05b6fc86a38def16705373f65f
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-05-05 14:17:12 -0400

    .

--- src/pokemon-detail.js
+++ src/pokemon-detail.js
@@ -23,16 +23,16 @@ fetch("/all_pokemon_data_with_forms.json")
 
     // Build the Pokémon detail card
     detailContainer.innerHTML = `
-      <div class="pokemon-card">
+      <div class="pokemon_container">
+        <a href="pokemon-db.html">← Back to List</a>
         <img src="${pokemon.image}" alt="${pokemon.name}" />
-        <h1>${pokemon.name} <span>${pokemon.pokedex_number}</span></h1>
+        <h1 class="pokemon_info--name">${pokemon.name} <span>${pokemon.pokedex_number}</span></h1>
         <p><strong>Generation:</strong> ${pokemon.generation}</p>
-        <p><strong>Type:</strong> 
+        <p class="pokemon_type"><strong>Type:</strong> 
           <span class="${pokemon.type}">${pokemon.type}</span> 
           ${pokemon.pokemonSpacer || ""} 
           <span class="${pokemon.type2 || ""}">${pokemon.type2 || ""}</span>
         </p>
-        <a href="pokemon-db.html">← Back to List</a>
       </div>
     `;
   })
